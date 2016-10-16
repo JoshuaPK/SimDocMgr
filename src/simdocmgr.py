864683e4485d3561eec1eed8fbaf00c1cca0fabd
Joshua Kramer <joskra42@gmail.com>
2016-10-16 10:41:15 -0400

    Periodic commit

--- src/simdocmgr.py
+++ src/simdocmgr.py
@@ -19,90 +19,150 @@
 # Then enter the number of pages, attribute:value pairs, and tags
 
 import logging
-import urwid
 import subprocess
 import sqlite3
 import tempfile
 import os
 import shutil
+import npyscreen
 
 # Config Values:
 
 scanpagePrg = '/usr/bin/scanimage'
 scanpageOpts = '--format=TIFF --mode=Gray'
 
+# Look here for opts:
+# http://www.wizards-toolkit.org/discourse-server/viewtopic.php?t=23225
+
 convertPrg = '/usr/bin/convert'
 convertOpts = 'options'
 
 dataDir = '../data'
 
-logging.basicConfig(format='%(asctime)s %(message)s', filename='../log/simdocmgr.log', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', filename='../log/simdocmgr.log', level=logging.INFO)
+
+# Database Setup:
+
+dbFile = dataDir + '/simdocmgr.db'
+dbConn = sqlite3.connect(dbFile)
+dbCur = dbConn.cursor()
+
+# SQL Queries:
+
+sqlLookupTags = 'SELECT tag_text FROM doc_tags WHERE tag_text LIKE ?'
 
 # Code:
 
-def scanPages(nbrPages, outPath):
+class TagSelector(Autocomplete):
+    def auto_complete(self, input):
 
-    """ Scans nbrPages pages and combines them into a PDF with a temporary file name,
-        then puts the pdf in outPath """
+        global dbConn
 
-    global scanpagePrg, scanpageOpts, convertPrg, convertOps
 
-    tmpLoc = tempfile.mkdtemp(dir=dataDir)
-    os.chdir(tmpLoc)
-
-    for i in range(1, nbrPages):
-        p = subprocess.Popen([scanpagePrg, scanpageOpts], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        spOut, spErr = p.communicate()
-        tmpFP, tmpFN = tempfile.mkstemp(suffix='.tif',text=False)
-        logging.info('Scanning page ' + str(i) + ' into file ' + tmpFN)
-        os.write(tmpFP, spOut)
-        os.close(tmpFP)
-        logging.warning(spErr)
-        p = None
-        tmpFP = None
-        tmpFN = None
-
-        # TODO: Implement some kind of 'press any key' between pages
+        self.cursor_position=len(self.value)
 
         pass
 
-    # Now we use ImageMagick to glue all of the tiff files together into a pdf.
+class TitleTagSelector(titlefield.TitleText):
+    _entry_type = TagSelector
 
-    logging.info("Gluing together PDF's")
+class SimDocApp(npyscreen.NPSApp):
 
-    tmpFP, tmpFN = tempfile.mkstemp()
-    os.close(tmpFP)
-    tmpFN = tmpFN + '.pdf'
-    logging.info("Created filename for PDF: " + tmpFN)
+    def main(self):
 
-    p = subprocess.Popen([convertPrg, convertOpts], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    cvOut, cvErr = p.communicate()
-    p = None
+        MF = npyscreen.Form(name = "SIMple DOCument ManaGeR")
+        fldTags = MF.add(npyscreen.Autocomplete, name = "Tags:", )
+        fldAttribs = MF.add(npyscreen.TitleText, name = "Attributes:", )
 
-    logging.info(cvOut)
-    logging.warning(cvErr)
+        pass
 
-    # Now we should have a PDF in the data directory, if things worked as planned.
-    # Build the cpString- the command to copy the file to its final location.
 
-    cpString = tmpFN + ' ../' + outPath
-    logging.info("Copying file using args: " + cpString)
 
-    p = subprocess.Popen(['/usr/bin/cp', cpString], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    cpOut, cpErr = p.communicate()
-    p = None
+    def adjust_widgets(self):
 
-    logging.info(cpOut)
-    logging.warning(cpErr)
+        # This is called for every keypress
 
-    # Now, remove the temp directory.
-    os.chdir('..')
-    shutil.rmtree(tmpLoc)
+
+        pass
+    
+    def showError(errText):
+
+
+        pass
+
+    def waitForKey():
+
+
+        pass
+
+    def scanPages(nbrPages, outPath):
+
+        """ Scans nbrPages pages and combines them into a PDF with a temporary file name,
+            then puts the pdf in outPath """
+
+        global scanpagePrg, scanpageOpts, convertPrg, convertOps
+
+        tmpLoc = tempfile.mkdtemp(dir=dataDir)
+        os.chdir(tmpLoc)
+
+        for i in range(1, nbrPages):
+            p = subprocess.Popen([scanpagePrg, scanpageOpts], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            spOut, spErr = p.communicate()
+            tmpFP, tmpFN = tempfile.mkstemp(suffix='.tif',text=False)
+            logging.info('Scanning page ' + str(i) + ' into file ' + tmpFN)
+            os.write(tmpFP, spOut)
+            os.close(tmpFP)
+            logging.warning(spErr)
+            p = None
+            tmpFP = None
+            tmpFN = None
+
+            waitForKey() 
+
+            pass
+
+        # Now we use ImageMagick to glue all of the tiff files together into a pdf.
+
+        logging.info("Gluing together PDF's")
+
+        tmpFP, tmpFN = tempfile.mkstemp()
+        os.close(tmpFP)
+        tmpFN = tmpFN + '.pdf'
+        logging.info("Created filename for PDF: " + tmpFN)
+
+        p = subprocess.Popen([convertPrg, convertOpts], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        cvOut, cvErr = p.communicate()
+        p = None
+
+        logging.info(cvOut)
+        logging.warning(cvErr)
+
+        # Now we should have a PDF in the data directory, if things worked as planned.
+        # Build the cpString- the command to copy the file to its final location.
+
+        cpString = tmpFN + ' ../' + outPath
+        logging.info("Copying file using args: " + cpString)
+
+        p = subprocess.Popen(['/usr/bin/cp', cpString], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        cpOut, cpErr = p.communicate()
+        p = None
+
+        logging.info(cpOut)
+        logging.warning(cpErr)
+
+        # Now, remove the temp directory.
+        os.chdir('..')
+        shutil.rmtree(tmpLoc)
 
     # All done!
 
     pass
 
+def displayUI():
+
+
+
+    pass
 
 def doStuff():
 
